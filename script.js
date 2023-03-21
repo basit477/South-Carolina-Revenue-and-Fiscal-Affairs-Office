9a40136d5465d92eecd11641829bf84e1db15578
basit477 <1002abasit@gmail.com>
2023-03-21 21:10:13 +0500

    Updated header and alignment

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const btnEL = document.querySelector(".menu-btn");
-btnEL.addEventListener("click", () => {
-  btnEL.classList.add("menu-btn-active");
-});
+// const btnEL = document.querySelector(".menu-btn");
+// btnEL.addEventListener("click", () => {
+//   btnEL.classList.add("menu-btn-active");
+// });
+
+
+function menuToggle(x) {
+    x.classList.toggle("change");
+  }
